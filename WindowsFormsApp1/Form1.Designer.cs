c5ea506472d5433bdc925d0387b6637622107b93
akimegr <82463845+akimegr@users.noreply.github.com>
2022-01-29 17:13:14 +0300

    feat: add files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Качества = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "1.Аккуратность",
+            "2. Беспечность",
+            "3. Вдумчивость",
+            "4. Восприимчивость",
+            "5. Вспыльчивость",
+            "6. Гордость",
+            "7. Грубость",
+            "8. Гуманность ",
+            "9. Доброта",
+            "10. Жизнерадостность",
+            "11. Заботливость",
+            "12. Завистливость",
+            "13.Застенчивость",
+            "14. Злопамятность",
+            "15. Искренность",
+            "16. Капризность",
+            "17. Легковерие",
+            "18. Медлительность",
+            "19. Мечтательность",
+            "20. Мнительность",
+            "21. Мстительность\t",
+            "22. Надежность",
+            "23. Настойчивость",
+            "24. Нежность",
+            "25. Нерешительность",
+            "26. Несдержанность",
+            "27. Обаяние",
+            "28. Обидчивость",
+            "29. Осторожность",
+            "30. Отзывчивость",
+            "31. Подозрительность",
+            "32. Принципиальность",
+            "33. Педантичность",
+            "34. Радушие",
+            "35. Развязность",
+            "36. Рассудительность",
+            "37. Самокритичность",
+            "38. Сдержанность",
+            "39. Справедливость",
+            "40. Сострадание",
+            "41. Стыдливость",
+            "42. Практичность",
+            "43. Трудолюбие",
+            "44. Трусость",
+            "45. Убежденность",
+            "46. Увлеченность",
+            "47. Черствость",
+            "48. Эгоизм"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 31);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(238, 429);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(214, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0/20";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ВЫБРАТЬ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Fuchsia;
+            this.button2.Location = new System.Drawing.Point(123, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "обновить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(277, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(517, 429);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Качества
+            // 
+            this.Качества.AutoSize = true;
+            this.Качества.BackColor = System.Drawing.Color.DarkOrchid;
+            this.Качества.Location = new System.Drawing.Point(12, 8);
+            this.Качества.Name = "Качества";
+            this.Качества.Size = new System.Drawing.Size(72, 17);
+            this.Качества.TabIndex = 6;
+            this.Качества.Text = "качества:";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Fuchsia;
+            this.button3.Location = new System.Drawing.Point(422, 478);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 58);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "РЕЗУЛЬТАТ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Fuchsia;
+            this.button4.Location = new System.Drawing.Point(871, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(230, 74);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Информация о программе";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Fuchsia;
+            this.button5.Location = new System.Drawing.Point(871, 278);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(230, 81);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "ПРАВИЛА";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Fuchsia;
+            this.button6.Location = new System.Drawing.Point(871, 185);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(230, 73);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Цели теста";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1104, 548);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Качества);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Качества;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int l = 20;
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //label1.Text = checkedListBox1.SelectedIndexCollection.Count.ToString;
+            label1.Text =  checkedListBox1.CheckedItems.Count.ToString() + "/20";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = checkedListBox1.CheckedItems.Count.ToString() + "/20";
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] array = new string[checkedListBox1.CheckedItems.Count];
+
+            for(int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                array[i] = checkedListBox1.CheckedItems[i].ToString();
+            }
+
+            if (array.Length == l)
+            {
+                dataGridView1.RowCount = l;
+                dataGridView1.Columns[0].HeaderText = "My Header";
+                dataGridView1.Columns.Add("1", "Идеальное");
+                dataGridView1.Columns.Add("2", "Реальное");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = array[i];
+                    dataGridView1.Rows[i].Cells[1].Value = "";
+                    dataGridView1.Rows[i].Cells[2].Value = "";
+                    dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+                }
+                button3.Visible=true;
+                dataGridView1.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Выберите 20");
+                return;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int len = dataGridView1.Columns[1].Width;
+            int[] checkId = new int[l];
+            int[] checkRel = new int[l];
+            for(int i = 0; i < l; i++)
+            {
+                try
+                {
+                    checkId[i] = Convert.ToInt32((dataGridView1.Rows[i].Cells[1].Value.ToString()));
+                    if(Convert.ToInt32((dataGridView1.Rows[i].Cells[1].Value.ToString()))>20 || Convert.ToInt32((dataGridView1.Rows[i].Cells[1].Value.ToString())) < 1)
+                    {
+                        MessageBox.Show("Введите значения от 1 до 20");
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Проверьте длину 'Идельное'");
+                    return;
+                }
+                
+            }
+            for (int i = 0; i < l; i++)
+            {
+                try
+                {
+                    checkRel[i] = Convert.ToInt32((dataGridView1.Rows[i].Cells[2].Value.ToString()));
+                    if (Convert.ToInt32((dataGridView1.Rows[i].Cells[1].Value.ToString())) > 20 || Convert.ToInt32((dataGridView1.Rows[i].Cells[1].Value.ToString())) < 1)
+                    {
+                        MessageBox.Show("Введите значения от 1 до 20");
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Проверьте длину 'Реальное'");
+                    return;
+                }
+
+            }
+            bool tmp = true;
+            
+            for (int i = 0; i < l; i++)
+               {
+                    for (int z = i + 1; z < l; z++)
+                    {
+                        if (checkId[i] == checkId[z])
+                        {
+                            tmp = false;
+                            MessageBox.Show("Есть одинаковые значения " + checkId[i].ToString());
+                        }
+                    }
+               }
+            for (int i = 0; i < l; i++)
+            {
+                for (int z = i + 1; z < l; z++)
+                {
+                    if (checkRel[i] == checkRel[z])
+                    {
+                        tmp = false;
+                        MessageBox.Show("Есть одинаковые значения " + checkRel[i].ToString());
+                    }
+                }
+            }
+
+
+            if (tmp)
+            {
+                double sum = 0;
+               for (int i = 0; i < l; i++)
+                {
+                   sum+=Math.Pow(Math.Abs(checkRel[i] - checkId[i]), 2);
+                }
+                double r = 1 - 0.00075 * sum;
+                string res = "ВАШ РЕЗУЛЬТАТ: " + r.ToString() + "\r\n\r\n";
+                if (-0.37<=r && r <= 0.37)
+                {
+                    MessageBox.Show(res+ "Результат не менее -0,37 и не более +0,37 указывает на слабую связь (или вообще ее отсутствие)\r\n между представлениями человека о «Я-идеальном» и о «Я-реальном». \r\n Такой результат может быть показателем неправильных расчетов или же неадекватным представлением человека о своих идеальных и реальных качествах. ");
+                }
+                if(0.38<=r && r <= 1)
+                {
+                    MessageBox.Show(res + "Результат от +0,38 до +1 говорит о положительной связи между «Я-идеальным» и «Я-реальным».");
+                }
+                if(0.38<r && r <= 0.89)
+                {
+                    MessageBox.Show(res + "Значения от +0,39 до +0,89 трактуются как адекватная самооценка с тенденцией к завышению.");
+                }
+                if(0.89<r && r <= 1)
+                {
+                    MessageBox.Show(res + "Значения же от +0,89 до +1 зачастую указывают на неадекватно завышенную самооценку.");
+
+                }
+                if(-1<=r && -0.38 >= r)
+                {
+                    MessageBox.Show(res + "Результат от -0,38 до -1 указывает на отрицательную связь между «Я-идеальным» и «Я-реальным».\r\n У человека расходятся представления о том, каким он хочет быть и тем, какой он в реальности.\r\n Значение можно трактовать как тенденцию к заниженной самооценке, и чем ближе результат к -1, тем больше несоответствий. ");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" В первой колонке выбираются 20 качеств идеального человека(выбрать: два раза нажать на слово, должна поставится галочка в квадрат, или прямым нажатием на квадратик, должна поставится галочка в квадрат), чтобы убрать галочку нажмите на галочку снова. При выбирании нового качества счётчик обновляется автоматически, НО может быть проблема с авто увеличением счётчика. Поэтому, для проверки количества выбранных полей нажимается кнопка 'ОБНОВИТЬ'.\r\n После того, когда счётчик при последним обновлении показал цифру 20, жмём кнопку 'ВЫБРАТЬ'. \r\n У нас появляется новое поле. Напротив каждого качества в этом поле вводим значения в соответствии с заданием (ОБЯЗАТЕЛЬНО вводить только цифры без пробелов!). \r\n После ввода полей нажимаем 'результат' и смотрим наш результат. ");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Почему каждому человеку стоит пройти тестирование?\r\n\r\n\r\n Методика разработана известным советским психологом Сергеем Андреевичем Будасси в 1970 - х.Тест Будасси даже в наши дни успешно применяют психологи во всем мире. \r\n\r\n Анализ помогает определить «Я - концепцию», среднее значение «Я - реального» и «Я - идеального». Именно «Я - концепция» влияет на выбор типа поведения человека, который, в свою очередь, и определяет направление деятельности, поступки и коммуникации.\r\n\r\n Почему важно понимать свою «Я - концепцию»? Не все, что мы думаем о самих себе, есть на 100 % реальным.Некоторые аспекты нашей личности не воспринимаются сознанием и реализуются нами неосознанно.Тест Будасси позволяет определить уровень нашей самооценки, и на основе этих знаний корректировать свои поступки и решения.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Инструкция к тесту Будасси\r\n\r\n\r\n Перед вами 48 слов, обозначающих свойства личности. Далее, вам необходимо выполнить 3 шага:\r\n 1 шаг. Из списка качеств выберите 20, которые, по вашему мнению, имеет идеальный человек. Рядом с выбранными качествами в левой колонке поставьте галочку.\r\n 2 шаг. Теперь из этих 20 качеств выберите наиболее неприятные вам. Напротив каждого качества в колонке «Идеальное» (2 колонка) проставьте цифры от 1 до 20, где 1 — наиболее неприятное качество, 20 — наименее неприятное. \r\n 3 шаг. И последний шаг — отметьте из 20 качеств наименее характерные для вас. В колонке «Реальное» (3 колонка) проставьте цифру напротив каждого качества от 1 до 20, где 1 — наименее характерное качество, 20 — наиболее характерное. \r\n ЗНАЧЕНИЯ ОТ 1 ДО 20 НЕ ДОЛЖНЫ ПОВТОРЯТЬСЯ!!!\r\n\r\n После правильного выполнения всех пунктов смело жмите на 'результат'!");
+        }
+    }
+}
